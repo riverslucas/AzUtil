61f56742561aac312209ac67ff90ce9917789d2e
Nelson Apaza <riverslucas@yahoo.com>
2023-03-07 10:35:09 -0500

    Vasdf

---
+++ Util/Util/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp3;
+using Microsoft.Graph;
+using System.IO;
+
+namespace Util
+{
+    internal class Program
+    {
+        private static string tenantId = "";
+        private static string appId = "";
+        private static string secret = "";
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello, World!");
+                var test = new GraphClient(tenantId, appId, secret);
+                var res = await test.AddServicePrincipal(
+                    new ServicePrincipal() { DisplayName= "testmenow" }
+                    ) ;
+
+                Console.WriteLine(res);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
